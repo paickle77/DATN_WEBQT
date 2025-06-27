0f61773f993a3a184dfa28a0dab9652f4a1db5b0
hocbtph48716 <hocbuj2001@gmail.com>
2025-06-27 13:13:20 +0700

    update login web quản trị

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,4 +6,12 @@ const api = axios.create({
   headers: { 'Content-Type': 'application/json' }
 });
 
+api.interceptors.request.use(cfg => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    cfg.headers.Authorization = `Bearer ${token}`;
+  }
+  return cfg;
+});
+
 export default api;
